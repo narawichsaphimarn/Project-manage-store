03d3bff89d7b25457266b664153a6761380f3ab9
Narawich Saphimarn (Ball)\Naras <naras529@vanpl.postbox.in.th>
2021-02-22 22:04:27 +0700

    update mock

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 // force: true will drop the table if it already exists
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync with { alter: true }");
-  // require("./app/mock/index.mock")(db);
+  require("./app/mock/index.mock")(db);
 });
 
 var corsOptions = {
