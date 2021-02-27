11b01c95233bbe7f7b5bad588f835e3f5574ded7
Narongsak <54994033+sscarry2@users.noreply.github.com>
2021-02-27 15:08:35 +0700

    test commit

--- server.js
+++ server.js
@@ -2,21 +2,22 @@
 // ******************** MAIN Of App ************************* //
 // ********************************************************** //
 
-const express = require("express");
+const express = require('express');
 const app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const db = require("./app/config/db.config.js");
+const db = require('./app/config/db.config.js');
 const PORT = process.env.PORT || 3001;
 
 // force: true will drop the table if it already exists
 db.sequelize.sync({ force: true, alter: true }).then(() => {
-  console.log("Drop and Resync with { alter: true }");
-  require("./app/mock/index.mock")(db);
+  console.log('Drop and Resync with { alter: true }');
+  require('./app/mock/index.mock')(db);
 });
 
-require("./app/route/index.route")(app);
+console.log('test commmit');
+require('./app/route/index.route')(app);
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
