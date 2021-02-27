f8fca72e3eea9e6692ac8d7e33b2a481cef7313f
Narawich Saphimarn (Ball)\Naras <naras529>
2021-02-27 17:16:58 +0700

    update new base

--- app/config/env.js
+++ app/config/env.js
@@ -6,10 +6,10 @@ const env = {
   secret:
     "_^!Gs?&cU6UmPGDP_n#JG7aac+RxHGJJcc#a+7w2W6SzEZm9HXv2fX2%^E9WkA5sEz#W7w?BNv%NM6+h%JHesh^D7e5EYLceJHYkT$7XKv",
   staging: {
-    database: "heroku_5805ccaaddd02a0",
-    username: "bca0acb0721baa",
-    password: "d6149bc3",
-    host: "us-cdbr-east-03.cleardb.com",
+    database: "shop_market",
+    username: "naras",
+    password: "naras",
+    host: "fourdust.kozow.com",
     dialect: "mysql",
     pool: {
       max: 10,
@@ -19,10 +19,10 @@ const env = {
     },
   },
   local: {
-    database: "heroku_70d82bac1ba4d98",
-    username: "b6430c0ab77cc5",
-    password: "aad3aab3",
-    host: "us-cdbr-east-03.cleardb.com",
+    database: "shop_market",
+    username: "naras",
+    password: "naras",
+    host: "fourdust.kozow.com",
     dialect: "mysql",
     pool: {
       max: 10,
