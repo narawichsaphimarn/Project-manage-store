c44e353bbb0d8c89089d7b72d057fbd8722fe040
Narawich Saphimarn (Ball)\Naras <naras529>
2021-02-27 16:09:11 +0700

    update core

--- server.js
+++ server.js
@@ -2,23 +2,30 @@
 // ******************** MAIN Of App ************************* //
 // ********************************************************** //
 
-const express = require('express');
+const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-var cors = require('cors');
+var cors = require("cors");
 
-const db = require('./app/config/db.config.js');
+const db = require("./app/config/db.config.js");
 const PORT = process.env.PORT || 3001;
 
 // force: true will drop the table if it already exists
 db.sequelize.sync({ force: true, alter: true }).then(() => {
-  console.log('Drop and Resync with { alter: true }');
-  require('./app/mock/index.mock')(db);
+  console.log("Drop and Resync with { alter: true }");
+  require("./app/mock/index.mock")(db);
 });
 
-console.log('test commmit');
-require('./app/route/index.route')(app);
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+require("./app/route/index.route")(app);
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
