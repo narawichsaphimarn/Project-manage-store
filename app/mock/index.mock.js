307ed7398f8bc670b260fb972850b27c076874dc
Narawich Saphimarn (Ball)\Naras <naras529>
2021-03-26 21:53:50 +0700

    update mock

--- app/mock/index.mock.js
+++ app/mock/index.mock.js
@@ -4,8 +4,8 @@
 
 module.exports = async (db) => {
   await require("./role.mock")(db);
-  await require("./productGroup")(db);
+  // await require("./productGroup")(db);
   await require("./tradingRole.mock")(db);
   await require("./actMembership.mock")(db);
-  await require("./storeInformation.mock")(db);
+  // await require("./storeInformation.mock")(db);
 };
